e9914d3897532f5a9abd5053304ee5c11d5b4e69
yuye2nd <qegameqegame@qq.com>
2026-04-25 21:48:37 +0800

    Update runjava.sh

--- .github/workflows/script/runjava.sh
+++ .github/workflows/script/runjava.sh
@@ -1,8 +1,10 @@
 mkdir folia
 cd folia
 wget https://fill-data.papermc.io/v1/objects/f52c408490a0225611e67907a3ca19f7e6da2c6bc899e715d5f46844e7103c39/folia-1.21.11-14.jar
+ls
 java -server -Xms1G -Xmx12G -jar folia-1.21.11-14.jar
-git add
+cd ..
+git add .
 git commit -m "Testing java runner"
 git push
 
